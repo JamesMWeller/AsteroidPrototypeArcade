f1322280d3d0f737f44eb40ac8d9020b3d61df39
James weller <jamesmweller11@gmail.com>
2018-02-15 11:28:13 +0000

    Main resolution

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ var planets;
 var asteroid;
 var planet;
 var pendingDestroy = [];
-
 var planetLife = 3;
+
+var text;
 
 //var startLocation;
 
@@ -24,19 +25,21 @@ function preload() {
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	asteroids = game.add.physicsGroup(Phaser.Physics.ARCADE);
-	planets = game.add.physicsGroup(Phaser.Physics.ARCADE);
+    asteroids = game.add.physicsGroup(Phaser.Physics.ARCADE);
+    planets = game.add.physicsGroup(Phaser.Physics.ARCADE);
 
-	planets.enableBody = true;
-	asteroids.enableBody = true;
-    
+    planets.enableBody = true;
+    asteroids.enableBody = true;
+
+
+    text = game.add.text(16, 16, 'Your Planet has ' + planetLife + ' lives left.', { fill: '#ffffff' });
 
     for (var i = 0; i < 8; i++) {
         asteroid = asteroids.create(game.world.randomX, game.world.randomY, 'asteroid')
         {
             this.worldType = randomElement();
-			enableBody = true;
-			game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+            enableBody = true;
+            game.physics.enable(asteroid, Phaser.Physics.ARCADE);
         };
 
         var rand = game.rnd.realInRange(1, 3);
@@ -48,13 +51,13 @@ function create() {
         asteroid.input.useHandCursor = true;
         asteroid.events.onDragStart.add(onDragStart, asteroid);
         asteroid.events.onDragStop.add(onDragStop, asteroid);
-	
+
     }
 
     for (var i = 0; i < 1; i++) {
         planet = planets.create(game.width / 2, game.height / 2, 'planet');
         planet.scale.setTo(0.3, 0.3);
-        planet.body.setCircle(180);
+        planet.body.setCircle(345, 280, 375);
         planet.body.collideWorldBounds = true;
         planet.inputEnabled = true;
         planet.input.enableDrag();
@@ -63,15 +66,16 @@ function create() {
 }
 
 function update() {
-	while (pendingDestroy.length > 0){
-		pendingDestroy.pop().destroy();
-	}
-	if (game.physics.arcade.overlap(asteroids, planets, planetLoseLife)) {
-		console.log("test");
-	}
-	
-	  game.physics.arcade.overlap(asteroids, asteroids, addToPendingDestroy);
- 
+    while (pendingDestroy.length > 0) {
+        pendingDestroy.pop().destroy();
+    }
+    if (game.physics.arcade.overlap(asteroids, planets, planetLoseLife)) {
+        text.text = 'Your Planet has ' + planetLife + ' lives left.';
+    }
+
+
+    game.physics.arcade.overlap(asteroids, asteroids, addToPendingDestroy);
+
 }
 
 
@@ -99,34 +103,35 @@ function onDragStop() {
     console.log("dy = " + y);
     game.physics.arcade.moveToXY(this, x, y);
 }
-function addToPendingDestroy(a,b){
-	pendingDestroy.push(a);
-	pendingDestroy.push(b);
+function addToPendingDestroy(a, b) {
+    pendingDestroy.push(a);
+    pendingDestroy.push(b);
 }
 function overlapDestroy(a, b) {
 
-	var boundsA = a.getBounds();
+    var boundsA = a.getBounds();
     var boundsB = b.getBounds();
 
-	var x = (a.x + b.x) / 2;
+    var x = (a.x + b.x) / 2;
     var y = (a.y + b.y) / 2;
-	
-	//  create new planet here
 
-   // return Phaser.Rectangle.intersects(boundsA, boundsB);
+    //  create new planet here
+
+    // return Phaser.Rectangle.intersects(boundsA, boundsB);
 
 }
 
-function planetLoseLife(){
-	if(planetLife == 0){
-	gameOver();
-	} else if(planetLife > 0)
-	{
-	planetLife = planetLife -1;	
-	}
-	
+function planetLoseLife(a, b) {
+    if (planetLife == 0) {
+        gameOver();
+    } else if (planetLife > 0) {
+        pendingDestroy.push(a)
+        planetLife = planetLife - 1;
+    }
+
 }
 
-function gameOver(){
-	alert("Game Over!!");	
+function gameOver() {
+    alert("Game Over!!");
+    location.reload();
 }
